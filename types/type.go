cbf2b71cbcdedb815e7ddeb93ffcadd94cd89b8f
William Penington <will@eccles.willpenington.com>
2013-06-25 11:38:57 +1000

    Type System started
    In order to contain the mess of reflection magic spilling all over the code
    base, GoLX now has the beginings of a type system independent of Go's. It
    is designed to be easily interoperable with Go types and at this stage all
    GoLX types effectively have to be Go types as well.

---
+++ types/type.go
@@ -0,0 +1,69 @@
+/*
+GoLX Type system
+
+In order to avoid depending too heavily on Go's reflection and to confine the
+as much of the reflection magic as possible to one place GoLX has it's own type
+system for high level operations. This also unifies behaviour like fixture
+profiles into a classic inheritance scheme rather than having several ad hoc
+systems.
+
+The system is currently very simple and does not yet support inheritance or
+custom methods. All methods are found using Go reflection.
+*/
+package types
+
+import "reflect"
+
+type Type interface {
+	Name() string
+
+  // Embed the Object interface so all types are objects
+  Type() Type
+  Method(name string) Function
+}
+
+type Object interface {
+	Type() Type
+  Method(name string) Function
+}
+
+/* Simple type */
+type SimpleType struct {
+  Name string
+}
+
+func (st SimpleType) Name() string {
+  return st.Name
+}
+
+func (SimpleType) Type() Type {
+  return MetaType
+}
+
+func (st SimpleType) Method(name string) Function {
+  return Method(st, name)
+}
+
+/* Type of Type */
+/* All types should also be objects that give their type as the meta type */
+metaType := SimpleType{Name: "Type"}
+func MetaType() Type {
+  return metaType
+}
+
+/* Method calls */
+
+// Return the go method as a Function object if it uses Objects
+func Method(o Object, name string) Function {
+	rval := reflect.ValueOf(o)
+	rmethod := rval.MethodByName(name)
+
+	if rmethod.IsValid() {
+		f, err := NewFunction(rmethod.Interface())
+		if err != nil {
+			return f
+		}
+	}
+
+	return nil
+}
